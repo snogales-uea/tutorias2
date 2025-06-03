8b6290eb3c1316b2a7ae5f160d588643d712d462
santyng <s.nogales@outlook.com>
2025-06-03 18:38:15 -0500

    tutoria semana 2
    tutoria semana 2

---
+++ src/main/java/com/mycompany/tutorias2/Tutorias2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tutorias2;
+
+/**
+ *
+ * @author Santiago
+ */
+public class Tutorias2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World! tutoria semana 2");
+        
+    }
+}
